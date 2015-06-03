93242cd6bfe7e25ec72b500c286dc2e6194a8c96
Chris Penny <cpennny@gmail.com>
2015-06-03 19:56:00 -0400

    Initial Commit

---
+++ WaveformMixerTests/WaveformMixerTests.m
@@ -0,0 +1,40 @@
+//
+//  WaveformMixerTests.m
+//  WaveformMixerTests
+//
+//  Created by Chris Penny on 6/3/15.
+//  Copyright (c) 2015 Tufts University. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface WaveformMixerTests : XCTestCase
+
+@end
+
+@implementation WaveformMixerTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
